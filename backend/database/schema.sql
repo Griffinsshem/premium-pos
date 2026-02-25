80fb341bdab2e8809df7e964a27cba399bf085cb
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-02-25 06:50:31 +0300

    feat(db): create products table schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -7,3 +7,18 @@ CREATE TABLE users (
     role ENUM('admin','cashier') DEFAULT 'cashier',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Products Table
+CREATE TABLE products (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    sku VARCHAR(100) UNIQUE,
+    barcode VARCHAR(100),
+    category VARCHAR(100),
+    cost_price DECIMAL(10,2) NOT NULL,
+    selling_price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT DEFAULT 0,
+    supplier VARCHAR(150),
+    is_deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
